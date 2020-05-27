98e6f0fc7db0b7ded9cd2fc4f08f812cc7b8b06e
devindianushika <devindianushika@gmail.com>
2020-05-27 22:59:59 +0530

    modified BACKEND url

--- src/app/components/admin/registration/parent-register-form/parent-register-form.component.ts
+++ src/app/components/admin/registration/parent-register-form/parent-register-form.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit,Inject } from '@angular/core';
-import { MAT_DIALOG_DATA , MatDialogRef ,MatDialog, throwMatDuplicatedDrawerError } from '@angular/material';
-import { MatIconRegistry , MatIcon } from '@angular/material/icon';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, throwMatDuplicatedDrawerError } from '@angular/material';
+import { MatIconRegistry, MatIcon } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { FormControl, Validators, ValidatorFn ,AbstractControl , FormGroupDirective , NgForm } from '@angular/forms';
+import { FormControl, Validators, ValidatorFn, AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { RegistrationService } from 'src/app/services/registration/registration.service';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
@@ -18,89 +18,90 @@ import { ParentRegisterFormPartBComponent } from '../parent-register-form-part-b
 })
 export class ParentRegisterFormComponent implements OnInit {
 
-  hide : boolean;
+  hide: boolean;
 
-  parent : Parent = new Parent();
+  parent: Parent = new Parent();
 
-  isRegistrationNumberValid : boolean;
-  isFullNameValid : boolean;
-  isEmailValid : boolean;
+  isRegistrationNumberValid: boolean;
+  isFullNameValid: boolean;
+  isEmailValid: boolean;
 
   registrationNumberExists = false;
 
   email = new FormControl('', [Validators.required, Validators.email]);
-  regNo = new FormControl('',[Validators.required,this.regNoInvalid()]);
-  fullName = new FormControl('',[this.fullNameInvalid()]);
+  regNo = new FormControl('', [Validators.required, this.regNoInvalid()]);
+  fullName = new FormControl('', [this.fullNameInvalid()]);
 
-  
+
 
   constructor(
     public dialogRef: MatDialogRef<ParentRegisterFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ParentDialogData,private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,public dialog : MatDialog,
-    private registerService : RegistrationService) {
+    @Inject(MAT_DIALOG_DATA) public data: ParentDialogData, private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer, public dialog: MatDialog,
+    private registerService: RegistrationService) {
 
-      this.matIconRegistry.addSvgIcon(
-        "hide",
-        this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/hide_24dp.svg")
-      ).
+    this.matIconRegistry.addSvgIcon(
+      'hide',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/hide_24dp.svg')
+    ).
       addSvgIcon(
         "show",
-        this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/show_24dp.svg")
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/show_24dp.svg')
       );
 
-      this.hide = true;
+    this.hide = true;
 
-      // validation
-     this.isEmailValid = true;
-     this.isFullNameValid = false;
-     this.isRegistrationNumberValid =false;
+    // validation
+    this.isEmailValid = true;
+    this.isFullNameValid = false;
+    this.isRegistrationNumberValid = false;
 
-     this.registrationNumberExists = false;
-     
-    }
+    this.registrationNumberExists = false;
 
-  onCancelClick(){
+  }
+
+  onCancelClick() {
     this.dialogRef.close();
     console.log(this.parent.registrationNumber);
   }
 
-  ngOnInit(){
-    
+  ngOnInit() {
+
   }
 
-  getData() : void {
+  getData(): void {
 
     this.parent.registrationNumber = this.data.parent.registrationNumber;
     this.parent.fullName = this.data.parent.fullName;
     this.parent.email = this.data.parent.email;
 
-    this.registerService.parentExists(this.parent.registrationNumber).subscribe(x=> {
-        if(Boolean(x)){
-          this.registrationNumberExists = true;
-          alert("Parent Already exists!");
-        } 
-        else this.registrationNumberExists = false;
+    this.registerService.parentExists(this.parent.registrationNumber).subscribe(x => {
+      if (Boolean(x)) {
+        this.registrationNumberExists = true;
+        alert("Parent Already exists!");
+      }
+      else this.registrationNumberExists = false;
 
 
 
-        if(this.isFullNameValid && this.isRegistrationNumberValid && this.email.valid && !this.registrationNumberExists){
-          console.log(this.data.parent);
-          this.dialogRef.close();
-          this.openDialogForRegisterParentSecondForm();
+      if (this.isFullNameValid && this.isRegistrationNumberValid && this.email.valid && !this.registrationNumberExists) {
+        console.log(this.data.parent);
+        this.dialogRef.close();
+        this.openDialogForRegisterParentSecondForm();
       }
     });
 
-   
+
   }
 
 
-  openDialogForRegisterParentSecondForm() : void {
+  openDialogForRegisterParentSecondForm(): void {
     const dialogRef = this.dialog.open(ParentRegisterFormPartBComponent, {
       width: '300px',
-      disableClose : true,
-      data: { parent : this.parent
-    }
+      disableClose: true,
+      data: {
+        parent: this.parent
+      }
     });
   }
 
@@ -108,36 +109,36 @@ export class ParentRegisterFormComponent implements OnInit {
   // error messages
   getEmailErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            '';
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
   }
 
   getRegistrationNumberInvalidMessage() {
     return "Invalid registration number";
   }
 
-  getFullNameErrorMessage(){
+  getFullNameErrorMessage() {
     return "Cannot be empty";
   }
 
   regNoInvalid() {
-  return (control: AbstractControl): { [key: string]: boolean } | null =>{
-    if (Number(control.value) && control.value !== "" && control.value) {
-      this.isRegistrationNumberValid = true;
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (Number(control.value) && control.value !== "" && control.value) {
+        this.isRegistrationNumberValid = true;
         return null;
-    }else{
-      this.isRegistrationNumberValid = false;
-      return {'regNoInvalid': true};
-    }
-  };
+      } else {
+        this.isRegistrationNumberValid = false;
+        return { 'regNoInvalid': true };
+      }
+    };
   }
 
   fullNameInvalid() {
-    return (control: AbstractControl): { [key: string]: boolean } | null =>{
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (control.value !== "" && control.value) {
         this.isFullNameValid = true;
         return null;
-      }else{
+      } else {
         this.isFullNameValid = false;
         return { 'fullNameInvalid': true };
       }
--- src/app/modules/AppConstants.ts
+++ src/app/modules/AppConstants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
     public static get BACKEND_URL() : string {
-        return "http://192.168.8.101:8090/api/v1/";
+        return "http://localhost:8090//api/v1/";
     }
 }
